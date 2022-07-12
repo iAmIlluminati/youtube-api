5dcc2d060c6d23b40dac8ed1bb04c5f06875910f
KrishnanRGithub <vinokrish001@gmail.com>
2022-07-12 16:32:31 +0530

    works indevelopment mode, need to deploy

--- youPI/youPI/settings.py
+++ youPI/youPI/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'youPI.base.apps.BaseConfig',
+    'base.apps.BaseConfig',
     'rest_framework',
 ]
 
