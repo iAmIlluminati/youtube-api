2e5230304f055e53e614c01ed114604de50f8f6a
KrishnanRGithub <vinokrish001@gmail.com>
2022-07-12 14:47:40 +0530

    static path fix v2

--- youPI/youPI/settings.py
+++ youPI/youPI/settings.py
@@ -63,19 +63,17 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'youPI.urls'
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static'))
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'static'),
-# )
-
-# STATIC_URL  ='/static/'
-STATICFILES_DIRS = [
-    
-    BASE_DIR / "static"
-]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 TEMPLATES = [
     {
