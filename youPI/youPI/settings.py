80d1cb93a0ee8722288c55e4c95e6de5f75d0f45
KrishnanRGithub <vinokrish001@gmail.com>
2022-07-12 16:23:09 +0530

    deloyment changes

--- youPI/youPI/settings.py
+++ youPI/youPI/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'base.apps.BaseConfig',
+    'youPI.base.apps.BaseConfig',
     'rest_framework',
 ]
 
