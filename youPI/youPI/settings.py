8c0a208caf27a968f729fe99344ca73397c596d6
KrishnanRGithub <vinokrish001@gmail.com>
2022-07-12 15:35:50 +0530

    removed env

--- youPI/youPI/settings.py
+++ youPI/youPI/settings.py
@@ -11,14 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 
-import environ
 from pathlib import Path
 import os
-
-env = environ.Env()
-environ.Env.read_env()
-# Initialise environment variables
-# Loads the environment variables from .env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
